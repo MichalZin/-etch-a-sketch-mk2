c8da3d7b63628a83570688da8a8b1b8e6eeaf3a5
MichalZin <124308169+MichalZin@users.noreply.github.com.>
2024-06-19 13:44:27 +0200

    Stylistic edits and adding reset function

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
 const canvas = document.querySelector(".canvas");
 const inputZone = document.querySelector(".formInput");
-
+const resetButton = document.getElementById("reset");
+const numberCount = document.getElementById('inputNum');
 
 inputZone.addEventListener("submit", (e) => {
 
 
     e.preventDefault();
 
-    let numberCount = document.getElementById('inputNum').value;
+    
     const canvasWidth = parseInt(getComputedStyle(canvas).width);
     const canvasHeight = parseInt(getComputedStyle(canvas).height);
 
-    const repeatDiv = () => {
-        canvas.innerHTML = '';
-        for (let i=0; i<numberCount; i++) {
-            const newDiv = document.createElement("div");
-            newDiv.style.width = `${canvasWidth/numberCount}px`;
-            newDiv.style.height = `${canvasHeight/numberCount}px`;
-            newDiv.style.backgroundColor = 'red'
-            newDiv.style.border = "2px solid black";
-            canvas.appendChild(newDiv);
-        }
-            
-    }
-    repeatDiv()
-
+    
     const repeatRow = () => {
-        
+        canvas.innerHTML = '';
+        for (let i=0; i<numberCount.value; i++) {
+            const newRow = document.createElement("div");
+            repeatDiv(newRow);
+            canvas.appendChild(newRow);
+        }
+
     }
+    const repeatDiv = (parent) => {
+        
+        for (let i=0; i<numberCount.value; i++) {
+            const newDiv = document.createElement("div")
+            newDiv.style.width = `${canvasWidth/numberCount.value}px`;
+            newDiv.style.height = `${canvasHeight/numberCount.value}px`;
+            newDiv.classList.add("singleTile")
+            parent.appendChild(newDiv);
+        }
+    }
+
+    
+    repeatRow()
 });
+
+resetButton.addEventListener("click", () => {
+    numberCount.value = '';
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.lastChild)
+    }
+    ;
+
+})
